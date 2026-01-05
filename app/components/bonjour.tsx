20a26764c095d842c9435277d0904464f34f713f
mohya972 <mohya972n@gmail.com>
2026-01-05 16:17:44 -0400

    First commit

---
+++ app/components/bonjour.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Bonjour = () => {
+    return (
+        <View>
+            <Text> Bonjour Mohya ! </Text>
+        </View>
+    )
+}
+
+export default Bonjour
+
+const styles = StyleSheet.create({})
